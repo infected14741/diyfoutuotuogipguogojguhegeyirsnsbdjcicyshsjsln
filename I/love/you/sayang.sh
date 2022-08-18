4921342ebbbb274d51af04cb8bf89e8e6eab6777
infected14741 <42504662+infected14741@users.noreply.github.com>
2022-08-18 19:02:17 +0700

    Update sayang.sh

--- I/love/you/sayang.sh
+++ I/love/you/sayang.sh
@@ -10,3 +10,19 @@ python khdoudulfjfnfhksyodyfjvkhkw.py
 clear
 rm -rf khdoudulfjfnfhksyodyfjvkhkw.py
 clear
+cd
+clear
+cd indah
+clear
+cd mewanty
+clear
+cd aku
+clear
+cd rindu
+clear
+cd kamu
+clear
+rm -rf diyfoutuotuogipguogojguhegeyirsnsbdjcicyshsjsln
+clear
+cd indah
+clear
